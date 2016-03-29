a4608c2570e39ec4b5e7d3d3753f0028ac70f4b0
Shirupan <429379083@qq.com>
2016-03-29 09:38:53 +0800

    update main.css

--- x-account-server/src/main/com/account/service/AccountServlet.java
+++ x-account-server/src/main/com/account/service/AccountServlet.java
@@ -7,13 +7,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import javax.websocket.Session;
-
-import com.account.dao.impl.MyUserDaoImpl;
-import com.account.domain.MyUser;
-import com.account.json.LoginRsp;
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONObject;
 
 /**
  * Servlet implementation class LoginServlet
