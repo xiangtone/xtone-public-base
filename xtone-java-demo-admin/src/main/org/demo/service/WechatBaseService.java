5af8911c88190f7e96be74b87894bdc1ffd6f2bc
soybean217 <13565644@qq.com>
2016-06-27 13:36:05 +0800

    增加微信的自定义菜单接口提交功能

--- xtone-java-demo-admin/src/main/org/demo/service/WechatBaseService.java
+++ xtone-java-demo-admin/src/main/org/demo/service/WechatBaseService.java
@@ -3,9 +3,11 @@ package org.demo.service;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 
 import org.apache.log4j.Logger;
 import org.common.util.ConnectionService;
+import org.demo.info.WechatToken;
 
 public class WechatBaseService {
 
@@ -19,8 +21,8 @@ public class WechatBaseService {
     return instance;
   }
 
-  public String getToken(String appId) {
-    String result = "";
+  public WechatToken getToken(String appId) throws SQLException, IllegalArgumentException, IllegalStateException {
+    WechatToken result = new WechatToken();
     PreparedStatement ps = null;
     Connection con = null;
     ResultSet rs = null;
@@ -31,8 +33,17 @@ public class WechatBaseService {
       int m = 1;
       ps.setString(m++, appId);
       rs = ps.executeQuery();
-      while (rs.next()) {
-        result = rs.getString("token");
+      if (rs.next()) {
+        result.setAppId(rs.getString("appId"));
+        result.setToken(rs.getString("token"));
+        result.setNextTime(rs.getLong("nextTime"));
+        result.setLastModTime(rs.getLong("lastModTime"));
+        result.setValidTime(rs.getLong("validTime"));
+        if (!result.isInValidTime()) {
+          throw new IllegalStateException("Wechat token expired .appID:" + appId);
+        }
+      } else {
+        throw new IllegalArgumentException("Can not find token of  appId:" + appId);
       }
     } catch (Exception e) {
       // TODO Auto-generated catch block
@@ -50,7 +61,7 @@ public class WechatBaseService {
     return result;
   }
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws IllegalArgumentException, SQLException {
     LOG.debug(WechatBaseService.getInstance().getToken(""));
   }
 }
---
+++ xtone-java-demo-admin/src/main/org/demo/service/WechatMenuService.java
@@ -0,0 +1,111 @@
+package org.demo.service;
+
+import java.io.File;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.entity.EntityBuilder;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ContentType;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+import org.apache.log4j.Logger;
+import org.common.util.ConnectionService;
+import org.demo.info.WechatToken;
+
+public class WechatMenuService {
+
+  private static final Logger LOG = Logger.getLogger(WechatMenuService.class);
+
+  public String createOrUpdateBaseMenu(String appId) throws SQLException, IllegalArgumentException,
+      ClientProtocolException, IOException {
+    String result = "";
+    WechatToken token = WechatBaseService.getInstance().getToken(appId);
+    String menu = this.getBaseMenu(appId);
+    sendWechatInterface(menu, token);
+    return result;
+  }
+
+  private void sendWechatInterface(String menu, WechatToken token) throws ClientProtocolException, IOException {
+    CloseableHttpClient httpclient = HttpClients.createDefault();
+    try {
+      HttpPost httppost = new HttpPost("https://api.weixin.qq.com/cgi-bin/menu/create?access_token=" + token.getToken());
+
+      File file = new File("");
+
+      // InputStreamEntity reqEntity = new InputStreamEntity(new
+      // FileInputStream(file), -1,
+      // ContentType.APPLICATION_OCTET_STREAM);
+      // reqEntity.setChunked(true);
+      LOG.debug(menu);
+      HttpEntity entity = EntityBuilder.create().setContentEncoding("utf-8")
+          .setContentType(ContentType.APPLICATION_JSON).setText(menu).build();
+      // It may be more appropriate to use FileEntity class in this particular
+      // instance but we are using a more generic InputStreamEntity to
+      // demonstrate
+      // the capability to stream out data from any arbitrary source
+      //
+      // FileEntity entity = new FileEntity(file, "binary/octet-stream");
+      httppost.setEntity(entity);
+
+      LOG.debug("Executing request: " + httppost.getRequestLine());
+      CloseableHttpResponse response = httpclient.execute(httppost);
+      try {
+        LOG.debug("----------------------------------------");
+        LOG.debug(response.getStatusLine());
+        LOG.debug(EntityUtils.toString(response.getEntity()));
+      } finally {
+        response.close();
+      }
+    } finally {
+      httpclient.close();
+    }
+
+  }
+
+  private String getBaseMenu(String appId) throws SQLException, IllegalArgumentException {
+    String result = "";
+    PreparedStatement ps = null;
+    Connection con = null;
+    ResultSet rs = null;
+    try {
+      con = ConnectionService.getInstance().getConnectionForLocal();
+      String sql = "SELECT content FROM tbl_wechat_menus where appId=? and `menuId`='base';";
+      ps = con.prepareStatement(sql);
+      int m = 1;
+      ps.setString(m++, appId);
+      rs = ps.executeQuery();
+      if (rs.next()) {
+        result = rs.getString("content");
+      } else {
+        throw new IllegalArgumentException("Can not find token for appId:" + appId);
+      }
+    } catch (Exception e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    } finally {
+      if (con != null) {
+        try {
+          con.close();
+        } catch (Exception e) {
+          // TODO Auto-generated catch block
+          e.printStackTrace();
+        }
+      }
+    }
+    return result;
+  }
+
+  public static void main(String[] args) throws IllegalArgumentException, SQLException, ClientProtocolException,
+      IOException {
+    WechatMenuService wechatMenuService = new WechatMenuService();
+    wechatMenuService.createOrUpdateBaseMenu("wx19ac78d42392068c");
+  }
+}
